f5432fb7279be400b39749fdd53b76e5eba067f6
Rob Cobb <rwcobbjr@gmail.com>
2022-09-02 04:13:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Write your solution below the starter code
+# Follow the instructions in the tab to the right
+
+import random
+
+# Welcome the user to the game
+print("Welcome to rock, paper, scissors. Good luck!")
+
+# Make a choice for the computer player
+# Get a choice from the user
+# Compare the user and computer choice
+# Print the right message, based on the choices
