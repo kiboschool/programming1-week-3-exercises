9d8b590da20aea3d6cb20a926d431ac4a5e7d2f5
Rob Cobb <rwcobbjr@gmail.com>
2022-10-29 17:42:09 -0600

    cleanup tests

--- test_validator.py
+++ test_validator.py
@@ -1,71 +1,39 @@
-from unittest import mock
-from unittest.mock import Mock, patch
-from unittest import TestCase
+from unittest.mock import patch
 import unittest
 import sys
 
-
-class Test(TestCase):
-    @patch('builtins.exit')
+@patch('builtins.exit')
+class TestValidator(unittest.TestCase):
     @patch('builtins.print')
-    @patch('builtins.input', return_value=["pasuwo12!#$3", "baume763!!!"])
-    def test_password_happy_flow(self, mock_input, mock_print, mock_exit):
+    def check_password(self, password, expected_output, mock_print):
+        patcher = patch('builtins.input', return_value=[password])
+        patcher.start()
         import validator
         try:
-            mock_print.assert_any_call("Validation Succeeded!")
+            mock_print.assert_any_call(expected_output)
         finally:
             sys.modules.pop('validator')
+            patcher.stop()
 
-    @patch('builtins.exit')
-    @patch('builtins.print')
-    @patch('builtins.input', return_value=["pass", "check", "Yus?1'"])
-    def test_password_length(self, mock_input, mock_print, mock_exit):
-        import validator
-        try:
-            mock_print.assert_any_call("Validation Failed: Password length should be between 9 and 12")
-        finally:
-            sys.modules.pop('validator')
-        mock_exit.assert_called_with(0)
+    def test_password_happy_flow(self, mock_exit):
+        self.check_password("pasuwo12!#$3", "Validation Succeeded!")
+        self.check_password("baume763!!!", "Validation Succeeded!")
+        
+    def test_password_length(self, mock_exit):
+        self.check_password("pass", "Validation Failed: Password length should be between 9 and 12")
+        self.check_password("check", "Validation Failed: Password length should be between 9 and 12")
+        self.check_password("Yus?1'", "Validation Failed: Password length should be between 9 and 12")
     
-    @patch('builtins.exit')
-    @patch('builtins.print')
-    @patch('builtins.input', return_value=["passwo1235", "Yus?1ad''31"])
-    def test_password_special_chars(self, mock_input, mock_print, mock_exit):
-        import validator
-        try:
-            # sys.stdout.write(str( mock_print.call_args ) + '\n')
-            # sys.stdout.write(str( mock_print.call_args_list ) + '\n')
-            mock_print.assert_any_call("Validation Failed: You need to have a minimum of 3 special characters")
-        finally:
-            sys.modules.pop('validator')
-        mock_exit.assert_called_with(0)
+    def test_password_special_chars(self, mock_exit):
+        self.check_password("passwo1235",  "Validation Failed: You need to have a minimum of 3 special characters")
+        self.check_password("Yus?1ad''31",  "Validation Failed: You need to have a minimum of 3 special characters")
 
-    @patch('builtins.exit')
-    @patch('builtins.print')
-    # @patch('builtins.input', return_value=["pass123!#$#", "Yus?1''$&"])
-    @patch('builtins.input', return_value=["pass123!#$#"])
-    def test_password_alpha_chars(self, mock_input, mock_print, mock_exit):
-        import validator
-        try:
-            # sys.stdout.write(str( mock_print.call_args ) + '\n')
-            # sys.stdout.write(str( mock_print.call_args_list ) + '\n')
-            mock_print.assert_any_call("Validation Failed: You need to have a minimum of 5 alpha characters")
-        finally:
-            sys.modules.pop('validator')
-        mock_exit.assert_called_with(0)
+    def test_password_alpha_chars(self, mock_exit):
+        self.check_password("pass123!#$#", "Validation Failed: You need to have a minimum of 5 alpha characters")
     
-    @patch('builtins.exit')
-    @patch('builtins.print')
-    @patch('builtins.input', side_effect=["pasuwop12!#$", "Yus?1''$&qa"])
-    def test_password_numbers(self, mock_input, mock_print, mock_exit):
-        import validator
-        try:
-            # sys.stdout.write(str( mock_print.call_args ) + '\n')
-            # sys.stdout.write(str( mock_print.call_args_list ) + '\n')
-            mock_print.assert_any_call("Validation Failed: You need to have a minimum of 3 numbers")
-        finally:
-            sys.modules.pop('validator')
-        mock_exit.assert_called_with(0)
+    def test_password_numbers(self, mock_exit):
+        self.check_password("pasuwop12!#$", "Validation Failed: You need to have a minimum of 3 numbers")
+        self.check_password("Yus?1''$&qa", "Validation Failed: You need to have a minimum of 3 numbers")
     
 
 if __name__ == '__main__':
